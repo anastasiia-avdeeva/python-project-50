b71b1b409cdc2f6edc8b77301a5ccbb0ad198497
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2026-03-10 21:52:45 +0200

    refactor: refactor gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,17 @@
 import argparse
 
 
-def main():
+def createParser():
     parser = argparse.ArgumentParser(
         usage='gendiff [-h] first_file second_file',
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    return parser
+
+
+def main():
+    parser = createParser()
     parser.parse_args()
 
 
