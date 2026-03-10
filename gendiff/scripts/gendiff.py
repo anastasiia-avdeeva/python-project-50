02f79f8706a3aa7cf087388f6e45048d3857347d
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2026-03-10 20:49:25 +0200

    feat: implement CLI interface with argparse

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,13 @@
+import argparse
+
+
 def main():
-    print('check-check')
+    parser = argparse.ArgumentParser(
+        usage='gendiff [-h] first_file second_file',
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.parse_args()
 
 
 if __name__ == '__main__':
