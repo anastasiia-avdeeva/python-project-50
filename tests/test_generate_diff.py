36d81f790246e1b88fe94f6e681975d5d8370ad5
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2026-03-25 23:49:54 +0200

    chore: add test generate_diff for nested data

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -63,3 +63,118 @@ def test_generate_diff_one_empty_dict(dict1_flat):
 }'''
 
     assert generate_diff(dict1_flat, {}) == expected
+
+
+@pytest.fixture
+def dict1_nested():
+    return {
+        "common": {
+            "setting1": "Value 1",
+            "setting2": 200,
+            "setting3": True,
+            "setting6": {
+                "key": "value",
+                "doge": {
+                    "wow": ""
+                }
+            }
+        },
+        "group1": {
+            "baz": "bas",
+            "foo": "bar",
+            "nest": {
+                "key": "value"
+            }
+        },
+        "group2": {
+            "abc": 12345,
+            "deep": {
+                "id": 45
+            }
+        }
+    }
+
+
+@pytest.fixture
+def dict2_nested():
+    return {
+        "common": {
+            "follow": False,
+            "setting1": "Value 1",
+            "setting3": None,
+            "setting4": "blah blah",
+            "setting5": {
+                "key5": "value5"
+            },
+            "setting6": {
+                "key": "value",
+                "ops": "vops",
+                "doge": {
+                    "wow": "so much"
+                }
+            }
+        },
+        "group1": {
+            "foo": "bar",
+            "baz": "bars",
+            "nest": "str"
+        },
+        "group3": {
+            "deep": {
+                "id": {
+                    "number": 45
+                }
+            },
+            "fee": 100500
+        }
+    }
+
+
+EXPECTED_NEST = '''{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}'''
+
+
+def test_generate_diff_nested(dict1_nested, dict2_nested):
+    assert generate_diff(dict1_nested, dict2_nested) == EXPECTED_NEST
