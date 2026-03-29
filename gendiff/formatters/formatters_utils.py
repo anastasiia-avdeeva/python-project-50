7b10c50f8147c84138c99052025506f990813547
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2026-03-30 01:56:39 +0300

    chore: add function annotation to formatters_utils module

--- gendiff/formatters/formatters_utils.py
+++ gendiff/formatters/formatters_utils.py
@@ -1,3 +1,6 @@
-def _transform_val(value):
+from typing import Union
+
+
+def _transform_val(value: Union[int, float, str, bool, None]):
     from_to = {None: 'null', True: 'true', False: 'false'}
     return from_to.get(value, value)
