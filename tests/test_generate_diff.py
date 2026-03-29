b0fcbf9a36ed6d5b820fb0f77892f6d6bdbd3e3f
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2026-03-29 23:22:41 +0300

    chore: add tests for generate_diff with plain formatter

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -23,8 +23,7 @@ def dict2_flat():
     }
 
 
-def test_generate_diff_flat(dict1_flat, dict2_flat):
-    expected = '''{
+EXPECTED_FLAT_STYLISH = '''{
   - baz: null
   - follow: false
     host: hexlet.io
@@ -33,7 +32,10 @@ def test_generate_diff_flat(dict1_flat, dict2_flat):
   + timeout: 20
   + verbose: true
 }'''
-    assert generate_diff(dict1_flat, dict2_flat) == expected
+
+
+def test_generate_diff_stylish_flat(dict1_flat, dict2_flat):
+    assert generate_diff(dict1_flat, dict2_flat) == EXPECTED_FLAT_STYLISH
     assert dict1_flat == {
         "host": "hexlet.io",
         "timeout": 50,
@@ -48,12 +50,12 @@ def test_generate_diff_flat(dict1_flat, dict2_flat):
     }
 
 
-def test_generate_diff_empty_dicts():
+def test_generate_diff_stylish_empty_dicts_():
     expected = '{\n}'
     assert generate_diff({}, {}) == expected
 
 
-def test_generate_diff_one_empty_dict(dict1_flat):
+def test_generate_diff_stylish_one_empty_dict(dict1_flat):
     expected = '''{
   - baz: null
   - follow: false
@@ -130,7 +132,7 @@ def dict2_nested():
     }
 
 
-EXPECTED_NEST = '''{
+EXPECTED_NESTED_STYLISH = '''{
     common: {
       + follow: false
         setting1: Value 1
@@ -176,5 +178,35 @@ EXPECTED_NEST = '''{
 }'''
 
 
-def test_generate_diff_nested(dict1_nested, dict2_nested):
-    assert generate_diff(dict1_nested, dict2_nested) == EXPECTED_NEST
+def test_generate_diff_stylish_nested(dict1_nested, dict2_nested):
+    assert generate_diff(dict1_nested, dict2_nested) == EXPECTED_NESTED_STYLISH
+
+
+EXPECTED_FLAT_PLAIN = """Property 'baz' was removed
+Property 'follow' was removed
+Property 'proxy' was removed
+Property 'timeout' was updated. From '50' to '20'
+Property 'verbose' was added with value: true"""
+
+
+def test_generate_diff_plain_flat(dict1_flat, dict2_flat):
+    assert generate_diff(dict1_flat, dict2_flat,
+                         'plain') == EXPECTED_FLAT_PLAIN
+
+
+EXPECTED_NESTED_PLAIN = """Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]"""
+
+
+def test_generate_diff_plain_nested(dict1_nested, dict2_nested):
+    assert generate_diff(dict1_nested, dict2_nested,
+                         'plain') == EXPECTED_NESTED_PLAIN
