77a78a4d501116b8e188c71778e63f401ccb9749
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2026-03-30 03:17:39 +0300

    feat: set default format to 'stylish' in CLI and add format option support in CLI

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -12,6 +12,7 @@ def create_parser():
     parser.add_argument(
         '-f',
         '--format',
+        default='stylish',
         help='set format of output'
     )
     return parser
@@ -27,12 +28,14 @@ def read_and_parse_file(file_path: str) -> dict:
         read_file(file_path), extension=get_file_extension(file_path))
 
 
-def main():
+def main() -> None:
     parser = create_parser()
     args = parser.parse_args()
     file1_dict = read_and_parse_file(args.first_file)
     file2_dict = read_and_parse_file(args.second_file)
-    print(generate_diff(file1_dict, file2_dict))
+    format = args.format
+    diff = generate_diff(file1_dict, file2_dict, format)
+    print(diff)
 
 
 if __name__ == '__main__':
