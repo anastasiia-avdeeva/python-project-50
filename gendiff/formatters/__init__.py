dbd1cfbcc5f27e30b05f69e129a7e3ca4a7fb5e6
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2026-03-31 20:15:12 +0300

    refactor: change modules name for formatters

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,4 +1,4 @@
-from .plain import format_plain as plain
-from .stylish import format_stylish as stylish
+from .format_plain import format_plain as plain
+from .format_stylish import format_stylish as stylish
 
 __all__ = ("stylish", "plain")
